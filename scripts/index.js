4a9a5019103dd943ab1d8e5fdbf07b1a618b3994
yelder <yelder302@gmail.com>
2021-03-21 01:10:59 +0100

    Ejercicio 2 nivel 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
+'use strict';
 // Nivel 1 Ejercicio 1
+
+let currentJoke = document.querySelector('#content'); 
 function generarChiste() {
     fetch('https://icanhazdadjoke.com/', { 
 
@@ -7,10 +10,11 @@ function generarChiste() {
         },
         method: 'GET', //Se usa get para obtener los datos
     })
-        .then((response) => response.json()) 
+    .then((response) => response.json())
 
         .then(data => {
             console.log(data);
+            currentJoke.innerHTML = `${data.joke}`
         })
 
         .catch (function(err) { 

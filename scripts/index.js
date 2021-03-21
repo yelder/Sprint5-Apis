49d40f039eec834b83489f01dcf183e29f3641b2
yelder <yelder302@gmail.com>
2021-03-21 19:28:28 +0100

    Nivel 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 'use strict';
 // Nivel 1 Ejercicio 1
 
-let currentJoke = document.querySelector('#content'); 
+/*let currentJoke = document.querySelector('#content'); 
 function generarChiste() {
     fetch('https://icanhazdadjoke.com/', { 
 
@@ -21,5 +21,28 @@ function generarChiste() {
                console.error(err);
            });
 }
+*/
 
+//Nivel 3
+const url = ["http://api.icndb.com/jokes/random", "https://icanhazdadjoke.com"]; // Array diferents APIS
 
+const generarChiste = async () => {
+    let random = Math.floor(Math.random() * url.length); //Para usar cualquiera de las 2 url
+    let actualUrl = url[random]; 
+
+    let response = await fetch(actualUrl, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    let data = await response.json();
+    const joke = [data.value, data.joke]; 
+    p.innerText = joke[random];
+}
+let p = document.createElement("p");
+document.getElementById("joke").appendChild(p); 
+    
+// Cuando se da click al boton se llama a la función
+document.getElementById("botonChiste").addEventListener("click", () => {
+    generarChiste();
+});
